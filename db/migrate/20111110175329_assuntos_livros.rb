0ce67744a8cff6aca9993853679d615fd6fbcf65
Manutencao <alexandrejr_soares@yahoo.com.br>
2011-11-16 09:31:59 -0200

    Update db/migrate/20111110175329_assuntos_livros.rb

--- db/migrate/20111110175329_assuntos_livros.rb
+++ db/migrate/20111110175329_assuntos_livros.rb
@@ -1,7 +1,13 @@
 class AssuntosLivros < ActiveRecord::Migration
   def self.up
+    create_table :assuntos_livros, :id => false do |t|
+      t.references :assunto, :null => false
+      t.references :livro, :null => false
+    end
+
   end
 
   def self.down
+    drop_table :assuntos_livros
   end
 end
