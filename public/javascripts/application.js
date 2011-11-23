23370791321ab62d849ddec02223d947eb79b38a
Alexandre Soares <alexandrejr_soares@yahoo.com.br>
2011-11-23 07:40:59 -0200

    Tudo Alexandre

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,11 +1,35 @@
 $(document).ready(function() {
+/*
 $('#livro_autor_id,#livro_identificacao_id,#livro_localizacao_id').selectmenu({width: 550,style:'dropdown', menuWidth: 550});
-$('#livro_area_id, ,#livro_editora_id').selectmenu({width: 300,style:'dropdown', menuWidth: 300})
+$('#livro_area_id,#livro_editora_id').selectmenu({width: 300,style:'dropdown', menuWidth: 300})
 $('#livro_data_edicao').selectmenu({width: 100,style:'dropdown', menuWidth: 100})
+*/
 
+$('#livro_autor_id,#livro_identificacao_id,#livro_localizacao_id').multiselect({
+    multiple: false,
+    minWidth: 550,
+    header: false,
+    selectedText: "",
+    close: true,
+
+});
+$("#livro_data_edicao,#livro_area_id,#livro_editora_id").multiselect({
+    minWidth: 290,
+    multiple: false,
+    header: false,
+    selectedText: ""
+
+});
+$("#livro_data_edicao_1i").multiselect({
+    multiple: false,
+    header: false,
+    selectedText: "",
+    minWidth: 100
+});
 $("#livro_assunto_ids").multiselect({
    selectedText: "# of # selected",
-   noneSelectedText: 'Selecione os assuntos'
+   noneSelectedText: 'Selecione os assuntos',
+   header: false
 });
 $( "input:submit" ).button();
 $( "#datepicker" ).datepicker();
