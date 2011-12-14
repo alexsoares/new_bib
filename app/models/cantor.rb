70e4fd0e5e9e249c8af1451d379ad2cc8ff147b3
Alexandre Soares <alexandrejr_soares@yahoo.com.br>
2011-12-14 07:31:14 -0200

    Calculos

--- app/models/cantor.rb
+++ app/models/cantor.rb
@@ -1,6 +1,6 @@
 class Cantor < ActiveRecord::Base
 
-  has_and_belongs_to_many :musicas, :dependent => :destroy
+  has_and_belongs_to_many :musicas
 
 
 def before_save
--- app/models/musica.rb
+++ app/models/musica.rb
@@ -1,6 +1,6 @@
 class Musica < ActiveRecord::Base
   
-  has_and_belongs_to_many :cantores, :dependent => :destroy
+  has_and_belongs_to_many :cantores
   has_and_belongs_to_many :audio_visuais
 
 def before_save
