88465afbf94f86968774dfa7595223cff61d30b7
Alexandre Soares <alexandrejr_soares@yahoo.com.br>
2012-01-18 15:51:00 -0200

    Ajeitando tombos nulos

--- app/controllers/tombos_controller.rb
+++ app/controllers/tombos_controller.rb
@@ -5,10 +5,11 @@ class TombosController < ApplicationController
   end
 
   def select_user
-    @livros_per_user = Tombo.find_all_by_user_id(params[:tombo][:user], :include => [:user,:livro])
+    @no_livros_per_user = Tombo.find_all_by_user_id(params[:tombo][:user], :include => [:user,:livro], :conditions => ["livro_id not in (select id from livros)"])
+    @livros_per_user = Tombo.find_all_by_user_id(params[:tombo][:user], :include => [:user,:livro], :conditions => ["livro_id in (select id from livros)"])
     render :update do |page|
       page.replace_html 'livro_per_user', :partial => "per_user"
-      page.replace_html 'count', :text => "Total cadastrado: #{@livros_per_user.count} livros"
+      page.replace_html 'count', :text => "Total cadastrado: #{@livros_per_user.count + @no_livros_per_user.count} - #{@no_livros_per_user.count} = #{@livros_per_user.count} livros"
     end
   end
 
